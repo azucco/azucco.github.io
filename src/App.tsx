f717baed7af3f97779010f2625e461a386c1c4af
azucco <alessandro.zucco@gmail.com>
2023-03-28 20:59:52 +0200

    feat(header): Config - sass global variables

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
 import './App.scss';
-import './styles/_variables.scss';
 import Header from './components/header';
 
 function App() {
