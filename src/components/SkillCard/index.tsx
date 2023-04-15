1073af3307e654b0aadffe653e8bda84917f27fd
azucco <alessandro.zucco@gmail.com>
2023-04-15 23:00:39 +0200

    feat(skillls-proficency-rating): Proficency - completed

--- src/components/SkillCard/index.tsx
+++ src/components/SkillCard/index.tsx
@@ -1,39 +1,42 @@
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import Typography from '@mui/material/Typography';
+import { RatingProps } from '@mui/material/Rating';
 import Rating from '@mui/material/Rating';
 import { styled } from '@mui/material/styles';
+import Tooltip from '@mui/material/Tooltip';
 
 export type skillCardContent = {
-    title: string, 
-    topic: string, 
-    content: string;
-    rating: 1 | 2 | 3 | 4 | 5,
-    color: string;
+  title: string,
+  topic: string,
+  content: string;
+  rating: 1 | 2 | 3 | 4 | 5,
+  color: string;
 }
 
 export const id: string = 'skills';
 
-interface StyledRating {
+interface StyledRatingProps {
+  width: string;
   color: string;
 }
 
 const StyledRating = styled(
-  ({ color, ...other } : any) => <Rating {...other} />)({ // TODO: fix type
-  '& .MuiRating-decimal': {
-    width: '20%',
-  },
-  '& .MuiRating-iconFilled': {
-    backgroundColor: (props: StyledRating) => props.color,
-  },
-  '& .MuiRating-iconEmpty': {
-    backgroundColor: '#32a852',
-  },
-});
+  ({ width, color, ...other }: RatingProps & StyledRatingProps) => <Rating {...other} />)(({ width, color }: StyledRatingProps) => ({
+    '& .MuiRating-decimal': {
+      width: width,
+    },
+    '& .MuiRating-iconFilled': {
+      backgroundColor: color,
+    },
+    '& .MuiRating-iconEmpty': {
+      backgroundColor: 'rgba(0, 0, 0, 0.1)',
+    },
+  }));
 
 const RatingElement = () => <div style={{ height: '10px' }}></div>
 
-export function SkillCard({title, topic, content, rating, color} :skillCardContent) {
+export function SkillCard({ title, topic, content, rating, color }: skillCardContent) {
   return (
     <Card>
       <CardContent>
@@ -43,20 +46,19 @@ export function SkillCard({title, topic, content, rating, color} :skillCardConte
         <Typography variant="h5" component="div">
           {title}
         </Typography>
-        {/* <Typography sx={{ mb: 1.5 }} color="text.secondary">
-          adjective
-        </Typography> */}
+        <Typography sx={{ mb: 1.5 }} color="text.secondary">
+        </Typography>
         <Typography variant="body2">
           {content}
         </Typography>
       </CardContent>
       <StyledRating
+        width='20%'
         color={color}
         defaultValue={rating}
-        readOnly 
-        getLabelText={(value: number) => `${value} Heart${value !== 1 ? 's' : ''}`}
+        readOnly
         precision={0.5}
-        sx={{display: 'flex'}}
+        sx={{ display: 'flex' }}
         icon={<RatingElement />}
         emptyIcon={<RatingElement />}
       />
