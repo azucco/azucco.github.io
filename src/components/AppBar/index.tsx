d4d371bfaa507fcdd4affc412329b093b5ecdc5f
azucco <alessandro.zucco@gmail.com>
2023-05-14 22:30:30 +0200

    feature(AppBar): Mode Switch - Started

--- src/components/AppBar/index.tsx
+++ src/components/AppBar/index.tsx
@@ -15,12 +15,61 @@ import MenuItem from '@mui/material/MenuItem';
 import { MdOutlineBrightness1, MdOutlineBrightness2 } from "react-icons/md";
 import { BsLinkedin } from "react-icons/bs";
 import { useTheme } from '@mui/material/styles';
+import { styled } from '@mui/material/styles';
+import Switch from '@mui/material/Switch';
 
 const pages = ['skills', 'experience'];
 
 type AppBarProps = {
   toggleColorMode: Function,
 }
+
+const Android12Switch = styled(Switch)(({ theme }) => ({
+  padding: 8,
+  '& .MuiSwitch-track': {
+    borderRadius: 22 / 2,
+    '&:before, &:after': {
+      content: '""',
+      position: 'absolute',
+      top: '50%',
+      transform: 'translateY(-50%)',
+      width: 16,
+      height: 16,
+    },
+    // '&:before': {
+    //   backgroundImage: `url('data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" height="16" width="16" viewBox="0 0 24 24"><path fill="${encodeURIComponent(
+    //     theme.palette.getContrastText(theme.palette.primary.main),
+    //   )}" d="M21,7L9,19L3.5,13.5L4.91,12.09L9,16.17L19.59,5.59L21,7Z"/></svg>')`,
+    //   left: 12,
+    // },
+    // '&:after': {
+    //   backgroundImage: `url('data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" height="16" width="16" viewBox="0 0 24 24"><path fill="${encodeURIComponent(
+    //     theme.palette.getContrastText(theme.palette.primary.main),
+    //   )}" d="M19,13H5V11H19V13Z" /></svg>')`,
+    //   right: 12,
+    // },
+  },
+  '& .MuiSwitch-thumb': {
+    boxShadow: 'none',
+    // color: 'green',
+    width: 16,
+    height: 16,
+    margin: 2,
+    // '&:before': {
+    //   backgroundImage: `url('data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" height="16" width="16" viewBox="0 0 24 24"><path fill="${encodeURIComponent(
+    //     theme.palette.getContrastText(theme.palette.primary.main),
+    //   )}" d="M21,7L9,19L3.5,13.5L4.91,12.09L9,16.17L19.59,5.59L21,7Z"/></svg>')`,
+    //   // left: 12,
+    // },
+    '&:after': {
+      content: '"TEST"'
+      // backgroundImage: `url('data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" height="16" width="16" viewBox="0 0 24 24"><path fill="${encodeURIComponent(
+      //   theme.palette.getContrastText(theme.palette.primary.main),
+      // )}" d="M19,13H5V11H19V13Z" /></svg>')`,
+      // right: 12,
+    },
+  },
+}));
 
 function ResponsiveAppBar({ toggleColorMode }: AppBarProps) {
   const [anchorElNav, setAnchorElNav] = useState<null | HTMLElement>(null);
@@ -151,6 +200,14 @@ function ResponsiveAppBar({ toggleColorMode }: AppBarProps) {
             ))}
           </Box>
 
+          <Box sx={{ flexGrow: 0 }}>
+            <Tooltip title={`Toggle ${theme.palette.mode === 'dark' ? 'light' : 'dark'} mode`}>
+              <Android12Switch defaultChecked onClick={() => {
+                setIsTransitionEnabled(false);
+                toggleColorMode();
+              }} />
+            </Tooltip>
+          </Box>
           <Box sx={{ flexGrow: 0 }}>
             <Tooltip title={`Toggle ${theme.palette.mode === 'dark' ? 'light' : 'dark'} mode`}>
               <IconButton onClick={() => {
