dc03e0ac0d4cf02772064daff2ccee3d2bc6bbaf
azucco <alessandro.zucco@gmail.com>
2023-06-09 22:02:20 +0200

    2e67c80: feat(ET): Layout - Rensponsive font size

--- src/components/ExperienceTimeline/index.tsx
+++ src/components/ExperienceTimeline/index.tsx
@@ -5,30 +5,34 @@ import TimelineConnector from '@mui/lab/TimelineConnector';
 import TimelineContent from '@mui/lab/TimelineContent';
 import TimelineOppositeContent, {
     timelineOppositeContentClasses,
-  } from '@mui/lab/TimelineOppositeContent';
+} from '@mui/lab/TimelineOppositeContent';
 import TimelineDot from '@mui/lab/TimelineDot';
 import Typography from '@mui/material/Typography';
+import { useTheme } from '@mui/material/styles';
 
 import * as data from '../../data';
 import { OverridableComponent } from '@mui/material/OverridableComponent';
-import { SvgIconTypeMap } from '@mui/material';
+import { SvgIconTypeMap, useMediaQuery } from '@mui/material';
 
 export type ExperienceTimelineItem = {
-    years: string; 
+    years: string;
     Icon: OverridableComponent<SvgIconTypeMap<{}, "svg">> & {
         muiName: string;
-    };  
-    company: string;  
-    job: string;  
-    description: string; 
-    type: 'focusOn' | 'beyond'; 
+    };
+    company: string;
+    job: string;
+    description: string;
+    type: 'focusOn' | 'beyond';
 }
 
 export const id: string = 'experience';
 
-function ExperienceTimelineItem ({years, Icon, company, job, description, type, isTop} :ExperienceTimelineItem & { isTop :boolean }) :JSX.Element {
+function ExperienceTimelineItem({ years, Icon, company, job, description, type, isTop }: ExperienceTimelineItem & { isTop: boolean }): JSX.Element {
+
+    const theme = useTheme();
+
     return (
-        <TimelineItem position={ type === 'focusOn' ? 'left' : 'right'}>
+        <TimelineItem position={type === 'focusOn' ? 'left' : 'right'}>
             <TimelineOppositeContent
                 sx={{ m: 'auto 0' }}
                 align="right"
@@ -38,28 +42,32 @@ function ExperienceTimelineItem ({years, Icon, company, job, description, type, 
                 {years}
             </TimelineOppositeContent>
             <TimelineSeparator>
-                { !isTop && <TimelineConnector />}
+                {!isTop && <TimelineConnector />}
                 <TimelineDot>
-                <Icon />
+                    <Icon />
                 </TimelineDot>
                 <TimelineConnector />
             </TimelineSeparator>
             <TimelineContent sx={{ py: '12px', px: 2 }}>
-                <Typography variant="h6" component="span">
-                {company}
+                <Typography sx={{ fontSize: useMediaQuery(theme.breakpoints.down('sm')) ? '1.1rem' : '1.25rem' }}>
+                    {company}
                 </Typography>
-                <Typography>{job}</Typography>
-                <Typography>{description}</Typography>
+                <Typography>
+                    {job}
+                </Typography>
+                <Typography sx={{ fontSize: useMediaQuery(theme.breakpoints.down('sm')) ? '0.80rem' : '0.95rem' }}>
+                    {description}
+                </Typography>
             </TimelineContent>
         </TimelineItem>
     )
 }
 
-export function ExperienceTimeline () {
+export function ExperienceTimeline() {
     return (
         <Timeline>
             {
-                data.experiences.map((experience, index) => <ExperienceTimelineItem {...experience } isTop={index === 0} key={index}></ExperienceTimelineItem>)
+                data.experiences.map((experience, index) => <ExperienceTimelineItem {...experience} isTop={index === 0} key={index}></ExperienceTimelineItem>)
             }
         </Timeline>
     )
